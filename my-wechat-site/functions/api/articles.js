7bada30a9672af08f37bf7b7b12308c037c231f0
classcommittee <z10091@sina.com>
2026-08-09 16:45:40 +0800

    Delete my-wechat-site directory

--- my-wechat-site/functions/api/articles.js
+++
@@ -1,6 +0,0 @@
-// functions/api/articles.js
-export function onRequest(context) {
-  return new Response(JSON.stringify({ success: true, data: [] }), {
-    headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' }
-  });
-}
